8f9b66cf40e2c35641b850ae1b23182e09ebeb69
kaushikjagadeshan <kausihk.jagadeshan@gmail.com>
2025-02-02 12:27:37 +0530

    Initial commit - Fresh upload of project

---
+++ src/components/HomePage/AboutSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AboutSection.css";
+
+function AboutSection() {
+  return (
+    <section className="about-section">
+     
+    </section>
+  );
+}
+
+export default AboutSection;
---
+++ src/components/Products/items/GearedParts.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ProductPage from "../ProductPages";
+import productImage from "../../assets/gearedparts.jpg";
+
+const GearedParts = () => {{
+    return <ProductPage title="GearedParts" image={productImage} description="High-quality GearedParts for industrial applications." />;
+}};
+
+export default GearedParts;
